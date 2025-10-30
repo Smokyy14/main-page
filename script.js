b017550d849d64925a618904654a771c94fafd51
Smokyy14 <fdsmdfr985@gmail.com>
2025-10-30 17:49:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   const moonbotButton = document.querySelector('.btn.primary');
-  const githubButton = document.querySelector('.btn.secondary');
-  const contactButton = document.querySelector('.btn.tertiary');
+  const projectsButton = document.querySelector('.btn.secondary');
+  const donateButton = document.querySelector('.btn.tertiary');
 
   if (moonbotButton) {
     moonbotButton.addEventListener('click', () => {
@@ -10,17 +10,16 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 
-  if (githubButton) {
-    githubButton.addEventListener('click', () => {
-      window.open('https://github.com/Smokyy14/discord.js-bot', '_blank');
+  if (projectsButton) {
+    projectsButton.addEventListener('click', () => {
+      window.location.href = 'projects';
     });
   }
 
-  if (contactButton) {
-  contactButton.addEventListener('click', () => {
+  if (donateButton) {
+  donateButton.addEventListener('click', () => {
     window.open('https://ko-fi.com/smokyydev', '_blank');
   });
 }
 
 });
-
